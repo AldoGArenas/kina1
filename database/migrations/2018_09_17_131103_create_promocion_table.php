2b6d8fda4df0621f9d47583bc6b16b4f3296a879
Karla Guzmán <isckg93@gmail.com>
2018-09-17 08:43:12 -0500

    creación de migraciónes, part1

---
+++ database/migrations/2018_09_17_131103_create_promocion_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePromocionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promocion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promocion');
+    }
+}
