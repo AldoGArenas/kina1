0ad8885e312fc9d99bf599e859d4d9349bd6396f
Aldo Arenas <uncubus.agaa@gmail.com>
2019-08-25 22:22:41 -0500

    cambios para front

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,20 @@
 @extends('main')
 @section('content')
 <section id="">
-    <div class="overlay">
+    <div class="header">
+    <div class="overlay"></div>
             <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-                    <source src="{{asset('video/intro1.mp4')}}" type="video/mp4">
-                </video>
+                <source src="{{asset('video/intro1.mp4')}}" type="video/mp4">
+            </video>
                 <div class="container h-100">
-                        <div class="d-flex h-100 text-center align-items-center">
-                          <div class="w-100 text-white">
-                            <h1 class="display-3">Video Header</h1>
-                            <p class="lead mb-0">With HTML5 Video and Bootstrap 4</p>
-                          </div>
+                    <div class="d-flex h-100 text-center align-items-end">
+                        <div class="w-100 text-white">
+                            <img src="{{asset('images/Industronic-Logo-2018.png')}}" alt="">
+                            {{--  <h1 class="display-5">MyR Material Eléctrico Reforma</h1>  --}}
+                            {{--  <p class="lead mb-0">Distribuidor autorizado Industronic</p>  --}}
                         </div>
-                      </div>
+                    </div>
+                </div>
         {{--  <div class="row">
             <div class="col-md-offset-2 col-md-8">
                 <h1 class="wow fadeIn" data-wow-offset="50" data-wow-delay="0.9s">Bienvenidos a:</h1>
@@ -30,48 +32,61 @@
 <!-- end home -->
 
 <!-- start about -->
-<section id="about">
+<section id="about" class="fondocontainer" >
     <div class="container">
+            <div class="acerca-de"></div>
         <div class="row">
-            <div class="col-md-12">
-                <h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">QUIENES<span> SOMOS</span></h2>
-
-                    <div class="col-sm-12 col-md-12 col-xs-12" style ="">
+            <div class="col-md-4">
+                {{--  <h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">QUIENES<span> SOMOS</span></h2>  --}}
+                <div class="col-sm-12 col-md-12 col-xs-12" style ="">
                     <img  src="images/Logo.png" class="img-responsive"  style="display: block; margin: auto; width:100px;">
-                    </div>
-                <div class="col-sm-12 col-md-12 col-xs-12" style=" margin-bottom:25px">
-                <p style="text-align:center; font-size: 18px"><span> MYR Material Eléctrico Reforma S.A. de C.V.</span> es una empresa distribuidora y comercializadora de una amplia gama de productos utilizados en el sector eléctrico, para instalaciones residenciales, industriales, comerciales y otras aplicaciones especializadas.</p>
                 </div>
-                <br>
+                <p style="text-align:center; font-size: 18px"><span> MYR Material Eléctrico Reforma S.A. de C.V.</span></p>
+                <p class="text-justify">Es una empresa distribuidora y comercializadora de una amplia gama de productos utilizados en el sector eléctrico, para instalaciones residenciales, industriales, comerciales y otras aplicaciones especializadas.</p>
+            </div>
+            {{--  <div class="col-sm-12 col-md-12 col-xs-12" style=" margin-bottom:25px">
+                
+                </div>
+                <br>  --}}
 
-            </div>
-
-            <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInUp" data-wow-offset="50" data-wow-delay="0.9s">
-                <div class="media">
-                    <div class="media-heading-wrapper">
-                        <div class="media-object pull-left">
-                            <i class="fa fa-rocket"></i>
+            <div class="col-md-4 my-auto">
+                <div class=" wow fadeInUp" data-wow-offset="50" data-wow-delay="0.9s">
+                    <div class="media">
+                        <div class="media-heading-wrapper">
+                            <div class="media-object pull-left">
+                                <i class="fa fa-rocket"></i>
+                            </div>
+                            <h3 class="media-heading">MISIÓN</h3>
                         </div>
-                        <h3 class="media-heading">MISIÓN</h3>
+                        <div class="media-body">
+                            <p style="font-size:16px">Dar el mejor servicio a nuestros clientes así como brindarles la seguridad que nuestra experiencia.</p>
+                        </div>
                     </div>
-                    <div class="media-body">
-                        <p style="font-size:16px">Dar el mejor servicio a nuestros clientes así como brindarles la seguridad que nuestra experiencia.</p>
+                </div>
+                <div class=" wow fadeInRight" data-wow-offset="50" data-wow-delay="0.6s">
+                    <div class="media">
+                        <div class="media-heading-wrapper">
+                            <div class="media-object pull-left">
+                                <i class="fa fa-building"></i>
+                            </div>
+                            <h3 class="media-heading">VISIÓN</h3>
+                        </div>
+                        <div class="media-body">
+                            <p style="font-size:16px"> Consolidarnos como una empresa altamente competitiva en el mercado nacional.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInRight" data-wow-offset="50" data-wow-delay="0.6s">
-                <div class="media">
-                    <div class="media-heading-wrapper">
-                        <div class="media-object pull-left">
-                            <i class="fa fa-building"></i>
-                        </div>
-                        <h3 class="media-heading">VISIÓN</h3>
-                    </div>
-                    <div class="media-body">
-                        <p style="font-size:16px"> Consolidarnos como una empresa altamente competitiva en el mercado nacional.</p>
-                    </div>
+            <div class="col-md-4">
+                <div class="col-md-12 text-center">
+                    <img class="iconoinicio" src="{{asset('images/customer-support.svg')}}" alt="">
+                </div>
+                <div class="col-md-12 text-center">
+                        <img class="iconoinicio" src="{{asset('images/delivery-man.svg')}}" alt="">
                 </div>
             </div>
+            
+           
         </div>
     </div>
 </section>
